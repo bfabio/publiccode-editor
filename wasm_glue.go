edb3103e28ae99257cc603213b7df9baf6067ca0
Fabio Bonelli <fb@fabiobonelli.it>
2020-02-05 12:00:46 +0100

    Add Wasm glue.

---
+++ wasm_glue.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/italia/publiccode-parser-go"
+)
+
+func IsPublicCodeYmlValid(this js.Value, args []js.Value) interface{} {
+	parser := publiccode.NewParser()
+	parser.DisableNetwork = true
+
+	err := parser.Parse([]byte(args[0].String()))
+	if err != nil {
+		fmt.Printf("validation ko:\n%v\n", err)
+		return js.ValueOf(false)
+	}
+	fmt.Println("validation ok")
+
+	return js.ValueOf(true)
+}
+
+func main() {
+	done := make(chan struct{}, 0)
+	js.Global().Set("IsPublicCodeYmlValid", js.FuncOf(IsPublicCodeYmlValid))
+	<-done
+}
